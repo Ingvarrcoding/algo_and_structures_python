cafc7563195e9732f0c2ce5471cb46b36d762f15
Igor Maslovskiy <ingvarrcoding@gmail.com>
2019-08-15 15:14:37 +0300

    ДЗ_5
    Выполнено

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,56 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+from collections import namedtuple
+
+enterprise = namedtuple('Enterprise', 'Name q1 q2 q3 q4')
+enterprises = []
+ent_count = int(input("Введите кол-во предприятий: "))
+
+for e in range(ent_count):
+    name = input('Введите наименование предприятия: ')
+    quarters = input('Введите прибыль в каждом квартале через пробел: ')
+    quarters_float = [float(x) for x in quarters.split()]
+    enterprises.append(enterprise(name, *quarters_float[:4]))
+
+aver_profit = namedtuple('Average_profit', 'Enterprise Value')
+aver_profits = []
+
+for ent in enterprises:
+    sum_profits = 0
+    for i in range(1, 5):
+        sum_profits += ent[i]
+    aver_profits.append(aver_profit(ent.Name, sum_profits/4))
+
+average = sum(ap.Value for ap in aver_profits) / len(aver_profits)
+
+more_aver = []
+less_aver = []
+
+for avr in aver_profits:
+    if avr.Value > average:
+        more_aver.append(avr.Enterprise)
+    elif avr.Value < average:
+        less_aver.append(avr.Enterprise)
+
+print()
+print('Предприятия с прибылью выше средней: ')
+print(', '.join(more_aver))
+print('Предприятия с прибылью ниже средней: ')
+print(', '.join(less_aver))
+
+'''
+Введите кол-во предприятий: 3
+Введите наименование предприятия: Гога
+Введите прибыль в каждом квартале: 12 10 13 18
+Введите наименование предприятия: Ваха
+Введите прибыль в каждом квартале: 9 17 23 10
+Введите наименование предприятия: Гиви
+Введите прибыль в каждом квартале: 11 12 13 14
+
+Предприятия с прибылью выше средней: 
+Ваха
+Предприятия с прибылью ниже средней: 
+Гога, Гиви
+'''
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,44 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+import collections
+
+def get_result():
+    input_num = input('Введите два числа в шестнадцатеричном формате через пробел: ').split()
+    numbers = collections.defaultdict(list)
+    for num in input_num:
+        numbers[num] = list(num)
+    sum_num = 0
+    mul_num = 1
+    for num in numbers.keys():
+        num = int(num, 16)
+        sum_num += num
+        mul_num *= num
+    sum_num = list(hex(sum_num).upper())[2:]
+    mul_num = list(hex(mul_num).upper())[2:]
+    return sum_num, mul_num
+
+def main():
+    try:
+        sum_num, mul_num = get_result()
+        print()
+        print(f'Сумма чисел {sum_num} \nПроизведение {mul_num}')
+    except ValueError:
+        print()
+        print('Неверный формат')
+
+main()
+
+'''
+Введите два числа в шестнадцатеричном формате через пробел: A2 C4F
+
+Сумма чисел ['C', 'F', '1'] 
+Произведение ['7', 'C', '9', 'F', 'E']
+
+----------------------------------------------------------------------
+
+Введите два числа в шестнадцатеричном формате через пробел: A2 и C4F
+
+Неверный формат
+'''
